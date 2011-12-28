ede8c0529a5081948cc5d39bfe6f806ecf9a7b0f
danielso2007 <danielso2007@64176f54-9834-11de-b5b0-958946df26fc>
2011-12-28 12:26:09 +0000

    Share project "exemploSeamMavenTomCatZero" into "https://projetofinal2009.googlecode.com/svn"

---
+++ src/main/java/br/com/teste/model/User.java
@@ -0,0 +1,123 @@
+package br.com.teste.model;
+
+import static org.jboss.seam.ScopeType.SESSION;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.Length;
+import org.hibernate.validator.NotNull;
+import org.hibernate.validator.Pattern;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Entity
+@Name("user")
+@Scope(SESSION)
+@Table(name = "User")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 7920783596220289055L;
+	@Id
+	@Length(min = 5, max = 15)
+	@Pattern(regex = "^\\w*$", message = "Não é um nome de usuário válido!")
+	private String username;
+	@NotNull
+	@Length(min = 5, max = 15)
+	private String password;
+	@NotNull
+	@Length(max = 100)
+	private String name;
+
+	public User(final String username, final String password, final String name) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.name = name;
+	}
+
+	public User() {
+		super();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(final String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		User other = (User) obj;
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (password == null) {
+			if (other.password != null) {
+				return false;
+			}
+		} else if (!password.equals(other.password)) {
+			return false;
+		}
+		if (username == null) {
+			if (other.username != null) {
+				return false;
+			}
+		} else if (!username.equals(other.username)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", name=" + name + "]";
+	}
+
+}
