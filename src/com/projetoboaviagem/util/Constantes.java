f9f37cba1fa897669047c38c2cf6aaceb97cff3d
danielso2007 <danielso2007@gmail.com>
2013-09-10 22:11:48 +0000

    Edição e criação de viagens.

--- src/com/projetoboaviagem/util/Constantes.java
+++ src/com/projetoboaviagem/util/Constantes.java
@@ -3,4 +3,5 @@ package com.projetoboaviagem.util;
 public interface Constantes {
     public static final String DATA_MEDIO = "dd/MM/yyyy";
     public static final String DATA_PADRAO = "dd/MM/yyyy HH:mm:ss";
+    public static final String VIAGEM_ID = "id";
 }
